9318555696e7968bc5643277f5a8edfb177b7804
Ojas Gupta <ojasgupta4321@gmail.com>
2022-11-16 08:15:03 +1100

    Added day-2 contents.

---
+++ Day-2/Celsius_To_Far_Converter.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+let celsius_temp = 33;
+
+let far_temp = ((celsius_temp * 9)/5) + 32
+
+print("The current temperature is \(celsius_temp)°C/\(far_temp)°F")
+
---
+++ Day-2/Day-2.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+
+let firstPart = "Hello, "
+let secondPart = "World!"
+
+
+// String concatentation
+let greeting = firstPart + secondPart;
+
+print(greeting)
+
+let luggageCode = "123" + "45"
+
+let name = "Oj"
+let age = 22
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+
+print(message)
+
+print("5 x 5 is \(5 * 5)")
