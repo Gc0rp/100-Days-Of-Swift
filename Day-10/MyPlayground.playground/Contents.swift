37e031fa98a5c3a8e1c37072ff5f9ac18b2ddd32
Ojas Gupta <ojasgupta4321@gmail.com>
2022-11-25 17:41:17 +1100

    Added Day-10

---
+++ Day-10/MyPlayground.playground/Contents.swift
@@ -0,0 +1,112 @@
+import Cocoa
+
+// Structs
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+    
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+
+let mmlp = Album(title: "Marshall Mathers LP", artist: "Marshall Mathers", year: 2001)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(mmlp.title)
+print(wings.artist)
+
+mmlp.printSummary()
+wings.printSummary()
+
+
+struct Employee {
+    let name: String
+//    var vacationRemaining: Int
+    var vacationAllocated = 14
+    var vacationTaken = 0
+    
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+    // Mutating mens you can change the values inside a struct
+//    mutating func takeVacation(days: Int) {
+//        if vacationRemaining > days {
+//            vacationRemaining -= days
+//            print("I'm going on vacation!")
+//            print("Days remaining: \(vacationRemaining)")
+//        } else {
+//            print("Oops! There aren't enough days remaining.")
+//        }
+//    }
+    
+    
+
+}
+
+
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
+archer.vacationRemaining = 5
+print(archer.vacationAllocated)
+
+
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts")
+            print("Old value was: \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+
+print(player.number)
+
