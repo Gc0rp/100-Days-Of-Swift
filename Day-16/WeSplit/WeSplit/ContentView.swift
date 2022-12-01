5d913c32ff658c3c0b3081a4a430cca73799002e
Ojas Gupta <ojasgupta4321@gmail.com>
2022-12-01 11:21:45 +1100

    Added Day-16.

---
+++ Day-16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Oj Gupta on 1/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Select your student", selection:
+                        $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day-16/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ojas Gupta on 1/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
