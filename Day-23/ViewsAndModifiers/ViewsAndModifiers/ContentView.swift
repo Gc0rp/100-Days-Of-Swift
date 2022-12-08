780ac26ea1ae31e38efc8f2e1f2ed90957e28fb3
Ojas Gupta <ojasgupta4321@gmail.com>
2022-12-08 19:04:54 +1100

    Added Day-23.

---
+++ Day-23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Ojas Gupta on 8/12/2022.
+//
+
+import SwiftUI
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
+            
+    }
+}
+
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var useRedText = false
+
+    var body: some View {
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with swift")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
