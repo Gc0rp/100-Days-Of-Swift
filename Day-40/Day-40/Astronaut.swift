ff87b3df6256ad309905618fb8336040fb306082
Ojas Gupta <ojasgupta4321@gmail.com>
2022-12-26 19:40:11 +1100

    Added Day-40.

---
+++ Day-40/Day-40/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Day-40
+//
+//  Created by Ojas Gupta on 26/12/2022.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
