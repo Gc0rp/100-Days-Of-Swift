7000612d764eb986379b3783748754a92e88cc4b
Ojas Gupta <ojasgupta4321@gmail.com>
2022-12-30 14:04:43 +1100

    Added Day-44

---
+++ Day-44/Day-44/Day-44/Day_44App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_44App.swift
+//  Day-44
+//
+//  Created by Ojas Gupta on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day_44App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
