c6c549c21556ddc9037e3567e06b6751ed1f65d7
Ojas Gupta <ojasgupta4321@gmail.com>
2023-01-07 10:35:11 +1100

    Added Day-50.

---
+++ Day-50/Day-50/Day-50/Day_50App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_50App.swift
+//  Day-50
+//
+//  Created by Ojas Gupta on 7/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day_50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
