30979cdd4562d8fcaa3c73f48383ebf5152ecdf4
Ojas Gupta <ojasgupta4321@gmail.com>
2023-01-06 15:24:01 +1100

    Added Day-49.

---
+++ Day-49/Day-49/ContentView.swift
@@ -0,0 +1,125 @@
+//
+//  ContentView.swift
+//  Day-49
+//
+//  Created by Ojas Gupta on 6/1/2023.
+//
+
+import SwiftUI
+
+final class User: ObservableObject, Codable {
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    @Published var name = "Paul Hudson"
+    
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+
+
+struct ContentView: View {
+    
+    @State private var results = [Result]()
+    
+    @State private var username = ""
+    @State private var email = ""
+    
+    var body: some View {
+//        List(results, id: \.trackId) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.trackName)
+//                    .font(.headline)
+//                Text(item.collectionName)
+//            }
+//        }
+//        .task {
+//            await loadData()
+//        }
+        
+        
+        
+        
+        
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+//            if let image = phase.image {
+//                image
+//                    .resizable()
+//                    .scaledToFit()
+//            } else if phase.error != nil {
+//                Text("There was an error loading the image")
+//            } else {
+//                ProgressView()
+//            }
+//        }
+//        .frame(width: 200, height: 200)
+        
+        
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating")
+                }
+            }
+            .disabled(disableForm)
+        }
+    }
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // more code to come
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day-49/Day-49/Day_49App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_49App.swift
+//  Day-49
+//
+//  Created by Ojas Gupta on 6/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day_49App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
