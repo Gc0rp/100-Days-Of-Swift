2859a0076011a831bbeaf1e33358ffffb2f33da0
Ojas Gupta <ojasgupta4321@gmail.com>
2023-01-18 11:35:44 +0530

    Added Day-57.

--- Day-57/Day-57/ContentView.swift
+++ Day-57/Day-57/ContentView.swift
@@ -13,13 +13,31 @@ struct Student: Hashable {
 }
 
 struct ContentView: View {
-    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+    @Environment(\.managedObjectContext) var moc
+
+    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+
     var body: some View {
-        List(students, id: \.self) { student in
-                Text(student.name)
+        VStack {
+            List(wizards, id: \.self) { wizard in
+                Text(wizard.name ?? "Unknown")
+            }
+
+            Button("Add") {
+                let wizard = Wizard(context: self.moc)
+                wizard.name = "Harry Potter"
+            }
+
+            Button("Save") {
+                do {
+                    try self.moc.save()
+                } catch {
+                    print(error.localizedDescription)
+                }
             }
         }
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Day-57/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  Day-57
+//
+//  Created by Ojas Gupta on 18/1/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
