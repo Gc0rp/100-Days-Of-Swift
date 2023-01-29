d889b9c88619092ec01058cec1877ded62ff057e
Ojas Gupta <ojasgupta4321@gmail.com>
2023-01-29 11:47:11 +0530

    Added Day-63.

---
+++ Day-63-Instafilter/Day-63-Instafilter/Day_63_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_63_InstafilterApp.swift
+//  Day-63-Instafilter
+//
+//  Created by Ojas Gupta on 29/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day_63_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
