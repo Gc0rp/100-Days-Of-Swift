1fbd3929760cd50c156f477c0aa89edcea71a47e
Ojas Gupta <ojasgupta4321@gmail.com>
2023-02-01 12:03:58 +0530

    Added Day-66.

---
+++ Day-65-66/Day-65/Day_65App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_65App.swift
+//  Day-65
+//
+//  Created by Ojas Gupta on 31/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day_65App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
