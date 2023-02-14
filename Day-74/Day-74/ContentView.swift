f50d660aeb78fac8dbd58048b40e3287b9b4a590
Ojas Gupta <ojasgupta4321@gmail.com>
2023-02-14 15:05:43 +1100

    Added Day-74.

--- Day-74/Day-74/ContentView.swift
+++ Day-74/Day-74/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var value = 10
+    
     let pictures = [
         "ales-krivec-15949",
         "galina-n-189483",
@@ -25,15 +28,54 @@ struct ContentView: View {
     @State private var selectedPicture = Int.random(in: 0...3)
     
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPicture = Int.random(in: 0...3)
+        //        Image(pictures[selectedPicture])
+        //            .resizable()
+        //            .scaledToFit()
+        //            .onTapGesture {
+        //                selectedPicture = Int.random(in: 0...3)
+        //            }
+        //            .accessibilityLabel(labels[selectedPicture])
+        //            .accessibilityAddTraits(.isButton)
+        //            .accessibilityRemoveTraits(.isImage)
+        //    }
+        
+//        Image(decorative: pictures[0])
+//            .accessibilityHidden(true)
+//
+//        VStack {
+//            Text("Your score is")
+//            Text("1000")
+//                .font(.title)
+//        }
+//        .accessibilityElement(children: .ignore)
+//        .accessibilityLabel("Your score is \(1000)")
+        
+        VStack {
+            Text("Value: \(value)")
+            
+            Button("Increment") {
+                value += 1
             }
-            .accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+            
+            Button("Decrement") {
+                value -= 1
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -=1
+            default:
+                print("Not handled")
+            }
+        }
+
+        
     }
 }
 
